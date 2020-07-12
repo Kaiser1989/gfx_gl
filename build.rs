03b6e4ac6698e50f8f7f9f7a9d10254f7a43f2ad
Philipp Kaiser <philipp.kaiser@perfectpattern.de>
2020-07-12 11:41:02 +0200

    Added android bindings

--- build.rs
+++ build.rs
@@ -3,6 +3,7 @@ extern crate gl_generator;
 use gl_generator::{Api, Fallbacks, Profile, Registry};
 use std::{env, fs::File, path::Path};
 
+#[cfg(not(target_os = "android"))]
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
 
@@ -23,6 +24,26 @@ fn main() {
             "GL_EXT_texture_sRGB",
         ],
     )
+    .write_bindings(gl_generator::StructGenerator, &mut file)
+    .unwrap();
+}
+
+#[cfg(target_os = "android")]
+fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+
+    let dest = Path::new(&out_dir);
+
+    let mut file = File::create(&dest.join("gl_bindings.rs")).unwrap();
+
+    Registry::new(
+        Api::Gles2, 
+        (3, 3), 
+        Profile::Core, 
+        Fallbacks::All, 
+        [
+            // none
+        ])
     .write_bindings(gl_generator::StructGenerator, &mut file)
     .unwrap();
 }
